44cf76c479f2e50ff76afcf13f85c0343349d427
nkmr <tomnkmr0120@gmail.com>
2023-10-10 12:52:26 +0900

    first commit

--- app/database/migrations/2023_09_14_023922_create_todo_items_table.php
+++ app/database/migrations/2023_09_14_023922_create_todo_items_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todo_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->foreignId('user_id') // 外部キーを追加
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->text('title');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todo_items');
     }
 };
